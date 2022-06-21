02976a1b85d50cb4d900934772c30dd0a09d7cc1
Kuldeep139 <kuldeeprathod33@gmail.com>
2022-06-21 18:24:25 +0530

    assign4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/style.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './components/Home';
+import Students from './components/students';
+import Contact from './components/Contact';
+import NavigationBar from './components/NavBar';
+import AddStudents from './components/addStudent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <NavigationBar/>
+      <Routes>
+        <Route path ='Home' element={<Home/>}/>
+        <Route path ='Students' element={<Students/>}/>
+        <Route path ='Contact' element={<Contact/>}/>
+        <Route path ='AddStudents'element={<AddStudents/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
